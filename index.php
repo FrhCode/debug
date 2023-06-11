bc2b2cde678eff3274e06dd8170f86588b148e8e
FrhCode <farhan7534031b@gmail.com>
2023-06-11 23:23:58 +0700

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-// Check if mysqli is enabled
-if (extension_loaded('mysqli')) {
-    echo 'mysqli extension is enabled on this server.';
+// Check if mysqlnd is enabled
+if (extension_loaded('mysqlnd')) {
+    echo 'mysqlnd extension is enabled on this server.';
 } else {
-    echo 'mysqli extension is not enabled on this server.';
+    echo 'mysqlnd extension is not enabled on this server.';
 }
