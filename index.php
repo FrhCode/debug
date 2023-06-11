9f012b6ae802c7938c0a9a2a0cdc81977e3ffc35
FrhCode <farhan7534031b@gmail.com>
2023-06-11 23:19:39 +0700

    update

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-phpinfo();
+// Check if cURL is enabled
+if (function_exists('curl_version')) {
+    echo 'cURL is enabled on this server.';
+} else {
+    echo 'cURL is not enabled on this server.';
+}
