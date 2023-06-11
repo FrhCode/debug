09d2522e835679b3fdfd3b48944b4457fa7153fa
FrhCode <farhan7534031b@gmail.com>
2023-06-11 23:22:53 +0700

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-// Check if JSON is enabled
-if (extension_loaded('json')) {
-    echo 'JSON extension is enabled on this server.';
+// Check if mbstring is enabled
+if (extension_loaded('mbstring')) {
+    echo 'mbstring extension is enabled on this server.';
 } else {
-    echo 'JSON extension is not enabled on this server.';
+    echo 'mbstring extension is not enabled on this server.';
 }
